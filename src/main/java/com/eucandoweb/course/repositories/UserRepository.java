413378367b31e9bc5232afdfe8e671fbf9b42ef3
jotaB <joaobatista1999estudo@gmail.com>
2023-05-01 18:58:03 -0300

    Service Layer, Component Registration

--- src/main/java/com/eucandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/eucandoweb/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.eucandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.eucandoweb.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
 }
---
+++ src/main/java/com/eucandoweb/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.eucandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eucandoweb.course.entities.User;
+import com.eucandoweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional <User> optional = repository.findById(id);
+		return optional.get();
+	}
+	
+
+}
