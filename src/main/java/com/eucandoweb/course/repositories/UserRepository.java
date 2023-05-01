f402031f53bc35369bdb38976448ce54b33e2f29
jotaB <joaobatista1999estudo@gmail.com>
2023-05-01 13:19:00 -0300

    JPA Repository, Dependency Injection, Database Seeding

---
+++ src/main/java/com/eucandoweb/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eucandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eucandoweb.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
