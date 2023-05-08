e4e34649d8b76d8a0573b43956cc549c79ad7457
jotaB <joaobatista1999estudo@gmail.com>
2023-05-08 00:51:04 -0300

    Exception Handling update

--- src/main/java/com/eucandoweb/course/services/UserService.java
+++ src/main/java/com/eucandoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.eucandoweb.course.repositories.UserRepository;
 import com.eucandoweb.course.services.exceptions.DatabaseException;
 import com.eucandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,10 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
